785f85a56fe90bb09db81f1e1b17bce098c6a0b9
faduzin <eric.fadul@gmail.com>
2025-01-30 18:44:36 -0300

    data_preprocessing function renaming

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
-from .data_preprocessing import data_preprocessing, data_scale
+from .data_preprocessing import data_preprocess, data_scale
 from .clustering import clusterize, plot_elbow, evaluate_cluster, plot_clusters, pairplot
 from .utils import log_changes, save_dataframe, load_dataframe, plot_cluster_distribution, plot_boxplot_and_histogram, dataframe_info
 
 __all__ = [
-    "data_preprocessing", 
+    "data_preprocess", 
     "data_scale",
     "clusterize", 
     "plot_elbow", 
