23e8672fb395ba37dd6981fdea74170f952c7021
faduzin <eric.fadul@gmail.com>
2025-01-30 08:37:10 -0300

    preprocess

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-def data_preprocessing(file_path):
+def preprocess_mall_customers(file_path):
     df = pd.read_csv(file_path)
     df = pd.get_dummies(df, drop_first=True).astype(int)
     df = df.rename(columns={"Annual Income (k$)":"Annual Income", "Spending Score (1-100)":"Spending Score", "Gender_Male":"Gender"})
@@ -10,3 +10,8 @@ def data_preprocessing(file_path):
     df = scaler.fit_transform(df)
 
     return df
+
+def preprocess_wholesome_customers(file_path):
+    df = pd.read_csv(file_path)
+
+def scale()
