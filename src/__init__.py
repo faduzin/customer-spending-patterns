fa56b4a5ff6a1b8816061b6ada02ef24e37a1661
faduzin <eric.fadul@gmail.com>
2025-01-30 18:25:03 -0300

    function added in __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .data_preprocessing import data_preprocessing, data_scale
 from .clustering import clusterize, plot_elbow, evaluate_cluster, plot_clusters, pairplot
-from .utils import log_changes, save_dataframe, load_dataframe, plot_cluster_distribution
+from .utils import log_changes, save_dataframe, load_dataframe, plot_cluster_distribution, plot_boxplot_and_histogram
 
 __all__ = [
     "data_preprocessing", 
@@ -13,5 +13,6 @@ __all__ = [
     "log_changes", 
     "save_dataframe", 
     "load_dataframe", 
-    "plot_cluster_distribution"
+    "plot_cluster_distribution",
+    "plot_boxplot_and_histogram"
 ]
