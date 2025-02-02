2cd4554e5bcd2d7983a56e36a446c99b43d62ad9
faduzin <eric.fadul@gmail.com>
2025-02-01 22:29:24 -0300

    added function on init

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from .data_preprocessing import data_preprocess, data_scale
-from .clustering import clusterize, plot_elbow, evaluate_cluster, plot_clusters, pairplot, find_best_k
+from .clustering import clusterize, plot_elbow, evaluate_cluster, plot_clusters, pairplot, find_best_k, plot_cluster_count
 from .utils import log_changes, save_dataframe, load_dataframe, plot_cluster_distribution, plot_boxplot_and_histogram, dataframe_info, plot_correlations
 
 __all__ = [
@@ -11,6 +11,7 @@ __all__ = [
     "plot_clusters", 
     "pairplot",
     "find_best_k",
+    "plot_cluster_count",
     "log_changes", 
     "save_dataframe", 
     "load_dataframe", 
